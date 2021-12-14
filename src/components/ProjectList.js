57126d6ae48d9d88e9d299f22379cb998ebc1604
nicholas shawn freeman <nick_freeman65@yahoo.com>
2021-12-13 18:00:07 -0600

    commit tree 0f350d98279aba1e84ac978b0a681944deaf2a44

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,13 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const projectList = projects.map((list) => {
+    return <ProjectItem key={list.id} name={list.name} about={list.about} technologies={list.technologies} />;
+  });
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectList}</div>
     </div>
   );
 }
